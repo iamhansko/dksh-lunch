e3905c8cf1164d2814f17d1c800447723b69851c
iamhansko <iamhansko@kakao.com>
2024-09-07 01:17:32 +0900

    fix: Handle Weekends Exception

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const { Builder, By, until } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromeOptions = new chrome.Options();
 chromeOptions.addArguments("--headless");
 chromeOptions.addArguments("--disable-gpu");
 chromeOptions.addArguments("--no-sandbox");
+const now = new Date();
+const kst = new Date(now.getTime() + (9 * 60 * 60 * 1000));
+const weekday = kst.getUTCDay();
 
-/* GET home page. */
 router.get('/', async (req, res, next) => {
-
   let driver = await new Builder()
   .forBrowser('chrome')
   .setChromeOptions(chromeOptions)
   .build();
 
   try {
-      await driver.get('http://dankook.sen.hs.kr/index.do');
-      await driver.wait(until.elementLocated(By.xpath('//*[@id="section_11"]/div/div/div[2]/a')), 10000);
-      const menuElement = await driver.findElement(By.xpath('//*[@id="section_11"]/div/div/div[2]/a'));
-      const menu = await menuElement.getText();
-      const imageElement = await driver.findElement(By.xpath('//*[@id="index_board_mlsv_031_347547"]/div/div[1]/p[2]/img'));
-      const image = await imageElement.getAttribute('src');
-      res.send({ menu: menu, image: image });
+    await driver.get('http://dankook.sen.hs.kr/index.do');
+    const menuElement = ((weekday != 6) && (weekday != 0)) ? await driver.findElement(By.xpath('//*[@id="section_11"]/div/div/div[2]/a')) : "";
+    const menu = ((weekday != 6) && (weekday != 0)) ? await menuElement.getText() : "주말에는 급식 정보가 존재하지 않습니다.";
+    const imageElement = await driver.findElement(By.xpath('//*[@id="index_board_mlsv_031_347547"]/div/div[1]/p[2]/img'));
+    const image = await imageElement.getAttribute('src');
+    res.send({ menu: menu, image: image });
   } catch (error) {
-      res.send(error);
+    res.send(error);
   } finally {
-      driver.quit();
+    driver.quit();
   }
 });
 
